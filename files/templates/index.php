b6052eaa9894a8cc3d5fb74ae83ea90a7db9bf66
Yoan Blanc <yoan@dosimple.ch>
2016-07-22 16:50:40 +0200

    Cleanup the Laravel image.

--- files/templates/index.php
+++ files/templates/index.php
@@ -35,38 +35,43 @@ unset($_SERVER["MYSQL_PASSWORD"]);
 unset($_SERVER["POSTGRES_PASSWORD"]);
 
 // MySQL
-$conn = new PDO("mysql:host={$mysql['host']};".
-                "port={$mysql['port']};dbname={$mysql['dbname']}",
-                $mysql["username"], $mysql["password"]);
-echo "<!-- All the MySQL tables:\n";
-foreach ($conn->query("SHOW TABLES;") as $row) {
-    echo " -  " . $row[0] . "\n";
+try {
+  $conn = new PDO("mysql:host={$mysql['host']};".
+                  "port={$mysql['port']};dbname={$mysql['dbname']}",
+                  $mysql["username"], $mysql["password"]);
+  echo "<!-- All the MySQL tables:\n";
+  foreach ($conn->query("SHOW TABLES;") as $row) {
+      echo " -  " . $row[0] . "\n";
+  }
+  echo "-->\n\n";
+} catch(PDOException $e) {
+  echo "<!-- " . $e->getMessage() . " -->\n\n";
 }
-echo "-->\n\n";
 
 // Postgres
-$conn = new PDO("pgsql:host={$pgsql['host']};".
-                "port={$pgsql['port']};dbname={$pgsql['dbname']}",
-                $pgsql["username"], $pgsql["password"]);
-$conn->exec("SET search_path TO production;");
+try {
+  $conn = new PDO("pgsql:host={$pgsql['host']};".
+                  "port={$pgsql['port']};dbname={$pgsql['dbname']}",
+                  $pgsql["username"], $pgsql["password"]);
+  $conn->exec("SET search_path TO production;");
 
-echo "<!-- All the PostgreSQL tables:\n";
-foreach ($conn->query("select tablename from pg_tables") as $row) {
-    echo " -  " . $row[0] . "\n";
+  echo "<!-- All the PostgreSQL tables:\n";
+  foreach ($conn->query("select tablename from pg_tables") as $row) {
+      echo " -  " . $row[0] . "\n";
+  }
+  echo "-->\n\n";
+} catch(PDOException $e) {
+  echo "<!-- " . $e->getMessage() . " -->\n\n";
 }
-echo "-->\n\n";
-
-// Memcached
-$memcache = new Memcached();
-$memcache->addServer($_SERVER["MEMCACHED_HOST"], (int) $_SERVER["MEMCACHED_PORT"]);
-$memcache->set("foo", 1);
-$memcache->increment("foo");
-echo "<!-- Memcache: " . $memcache->get("foo") . "-->\n\n";
 
 // Redis
-$redis = new Redis();
-$redis->connect($_SERVER["REDIS_HOST"], (int) $_SERVER["REDIS_PORT"]);
-echo "<!-- Redis: " . $redis->ping() . " -->\n\n";
+try {
+  $redis = new Redis();
+  $redis->connect($_SERVER["REDIS_HOST"], (int) $_SERVER["REDIS_PORT"]);
+  echo "<!-- Redis: " . $redis->ping() . " -->\n\n";
+} catch(RedisException $e) {
+  echo "<!-- " . $e->getMessage() . " -->\n\n";
+}
 
 // Sending emails
 mail("test@test.com", "Test", "Hello World!");
--- scripts/boot.sh
+++ scripts/boot.sh
@@ -2,6 +2,11 @@
 
 set -xe
 
+# regen SSH keys on each boot
+if [ 0 -eq `ls /etc/ssh/ssh_host_* | wc -l` ]; then
+    dpkg-reconfigure openssh-server
+fi
+
 /usr/local/bin/setup.py
 
 exec /usr/bin/runsvdir -P /etc/service
