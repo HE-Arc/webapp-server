b67a641f3fc5ba2ba5d58b37a1d4bacef2627f5e
Yoan Blanc <yoan@dosimple.ch>
2017-06-20 15:54:51 +0200

    One user/group for all the students.
    K.I.S.S.
    
    Signed-off-by: Yoan Blanc <yoan@dosimple.ch>

--- scripts/setup.py
+++ scripts/setup.py
@@ -7,9 +7,8 @@ Create the environment, users with all the required files.
 """
 
 __author__ = "Yoan Blanc <yoan@dosimple.ch>"
-__version__ = "0.6.0"
+__version__ = "0.7.0"
 
-import re
 import os
 import csv
 import sys
@@ -17,7 +16,6 @@ import pwd
 import random
 import shutil
 import subprocess
-import unicodedata
 import multiprocessing
 
 from collections import namedtuple
@@ -27,89 +25,17 @@ env = Environment(loader=FileSystemLoader("/var/templates"))
 wwwdir = "/var/www"
 
 ADMIN = "teachers"
+STUDENTS = "/root/config/students.tsv"
+
+StudentRecord = namedtuple(
+    "StudentRecord", "lastname, firstname, email, classname, github, "
+    "image1, team1, image2, team2, comment, week"
+)
 
 
-def formatUserName(firstname):
-    """
-    Format the real name into a username
 
-    E.g.: Juan Giovanni Di Sousa Santos -> juan
-    """
-    # Keep only the first firstname
-    first = re.match("^(.+?)\\b", firstname, re.U).group(0).lower()
-
-    username = unicodedata.normalize("NFD", first)
-    username = username.replace(" ", "")
-    # http://stackoverflow.com/questions/517923/what-is-the-best-way-to-remove-accents-in-a-python-unicode-string/517974#517974
-    return "".join([c for c in username if not unicodedata.combining(c)])
-
-
-def init_user(username, groupname, **kwargs):
-    """As the user."""
-    p = pwd.getpwnam(username)
-    uid, gid = p.pw_uid, p.pw_gid
-    homedir = p.pw_dir
-
-    os.initgroups(groupname, gid)
-    os.setgid(gid)
-    os.setuid(uid)
-    os.chdir(homedir)
-
-    os.environ["USER"] = username
-    os.environ["HOME"] = homedir
-    os.environ["UID"] = str(uid)
-
-    paths = [("base/bash_profile", ".bash_profile"),
-             ("base/gitconfig", ".gitconfig"), ("base/pgpass", ".pgpass")]
-    config = kwargs["environ"].get("CONFIG", None)
-    if config == "Laravel":
-        paths.append(("laravel/README.md", "README.md"))
-    elif config == "Rails":
-        paths.append(("rails/README.md", "README.md"))
-    elif config == "Python":
-        paths.append(("python/README.md", "README.md"))
-    else:
-        paths.append(("base/README.md", "README.md"))
-    for tpl, dest in paths:
-        if not os.path.exists(dest):
-            render(tpl, dest, username=username, groupname=groupname, **kwargs)
-
-    # Chmod the postgres configuration
-    os.chmod(".pgpass", mode=0o0600)
-
-    # link /var/www to ~/www
-    os.symlink(wwwdir, "www")
-
-    # Create .ssh/authorized_keys
-    os.mkdir(".ssh")
-    os.chmod(".ssh", mode=0o0700)
-
-
-def create_user(username, groupname, comment):
-    """Create a UNIX user (the group must exist beforehand)."""
-    groups = ["users"]
-
-    subprocess.check_call(
-        [
-            "useradd", username, "-c", comment, "--create-home",
-            "--no-user-group", "--shell", "/bin/bash", "--groups",
-            ",".join(groups)
-        ],
-        stderr=subprocess.PIPE,
-        stdout=subprocess.PIPE)
-    subprocess.check_call(
-        [
-            "usermod", username, "--gid", groupname, "--groups",
-            "{},users".format(groupname)
-        ],
-        stderr=subprocess.PIPE,
-        stdout=subprocess.PIPE)
-    proc = subprocess.Popen(
-        ["chpasswd"],
-        stdin=subprocess.PIPE,
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE)
-    proc.communicate("{}:{}".format(username, pwgen()).encode("utf-8"))
+def check_call(command, env, stderr=sys.stderr, stdout=sys.stderr):
+    subprocess.check_call(command, env=env, stderr=stderr, stdout=stdout)
 
 
 def authorized_keys(username, github):
@@ -138,7 +64,16 @@ def init_group(groupname, **kwargs):
     os.initgroups(groupname, gid)
     os.setgid(gid)
     os.setuid(uid)
-    os.chdir(homedir)
+    os.chdir(wwwdir)
+
+    # Randomize the password, for good measure.
+    proc = subprocess.Popen(
+        ["chpasswd"],
+        stdin=subprocess.PIPE,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE)
+    password = "{:064x}".format(random.randrange(16**64))
+    proc.communicate("{}:{}".format(groupname, password).encode('utf-8'))
 
     config = kwargs["environ"].get("CONFIG", None)
     environ = kwargs["environ"].copy()
@@ -187,9 +122,6 @@ def init_group(groupname, **kwargs):
         if not os.path.exists(dest):
             render(tpl, dest, groupname=groupname, **kwargs)
 
-    def check_call(command, env, stderr=sys.stderr, stdout=sys.stderr):
-        subprocess.check_call(command, env=env, stderr=stderr, stdout=stdout)
-
     if config == "Rails":
         shutil.copy2("/var/templates/rails/app/public/nginx-puma.png",
                      "app/public")
@@ -217,21 +149,6 @@ def init_group(groupname, **kwargs):
     return homedir, uid, gid
 
 
-def create_group(groupname):
-    """Create the system user named after the group."""
-    subprocess.check_call(
-        [
-            "useradd", groupname, "-c", "GROUP", "--no-create-home",
-            "--home-dir", wwwdir, "--user-group", "--system"
-        ],
-        stdin=subprocess.PIPE,
-        stdout=subprocess.PIPE)
-    subprocess.check_call(
-        ["usermod", "--lock", groupname],
-        stdin=subprocess.PIPE,
-        stdout=subprocess.PIPE)
-
-
 def render(template, path, **kwargs):
     """Render a Jinja2 template into the given file."""
     if not os.path.exists(path):
@@ -240,20 +157,13 @@ def render(template, path, **kwargs):
             f.write(template.render(**kwargs))
 
 
-def pwgen(length=128):
-    """Generate a secure password."""
-    proc = subprocess.Popen(
-        ["pwgen", "--secure", "{}".format(length), "1"],
-        stdout=subprocess.PIPE)
-    return proc.communicate()[0].decode().strip()
-
-
 def main(argv):
     # Load global conf
     environ = os.environ
 
     groupname = environ["GROUPNAME"]
     config = environ["CONFIG"]
+    poweruser = environ.get("POWERUSER", "poweruser")
 
     # Global environment "variables"
     environ["MYSQL_HOST"] = environ.get("MYSQL_HOST", "mysql")
@@ -286,14 +196,6 @@ def main(argv):
         with open("/etc/container_environment/{0}".format(k), "w+") as f:
             f.write(v)
 
-    # Test if the group exists already
-    try:
-        p = pwd.getpwnam(groupname)
-        sys.stderr.write("Setup already done!\n")
-        return 0
-    except KeyError:
-        pass
-
     # Configure SSMTP
     subprocess.check_call(
         [
@@ -303,36 +205,18 @@ def main(argv):
         stdin=subprocess.PIPE,
         stdout=subprocess.PIPE)
 
-    # Create the group
-    create_group(groupname)
-    p = pwd.getpwnam(groupname)
-    uid, gid = p.pw_uid, p.pw_gid
-    # Ownership of existing files
-    os.chown(wwwdir, uid, gid)
-    for root, dirs, files in os.walk(wwwdir):
-        for d in dirs:
-            os.chown(os.path.join(root, d), uid, gid)
-        for f in files:
-            os.chown(os.path.join(root, f), uid, gid)
-    # Use ACL to set the rights.
-    subprocess.check_call(
-        ["setfacl", "-R", "-m", "group:{}:rwX".format(groupname), wwwdir],
-        stdin=subprocess.PIPE,
-        stdout=subprocess.PIPE)
-    subprocess.check_call(
-        ["setfacl", "-dR", "-m", "group:{}:rwX".format(groupname), wwwdir],
-        stdin=subprocess.PIPE,
-        stdout=subprocess.PIPE)
-
     # Init the group files
     p = multiprocessing.Process(
-        target=init_group, args=(groupname, ), kwargs=dict(environ=environ))
+        target=init_group, args=(poweruser, ), kwargs=dict(environ=environ))
     p.start()
     p.join()
+
+    assert p.exitcode == 0, "init_group failed."
 
     # symlink the server config
     os.symlink("/var/www/config/nginx.conf",
                "/etc/nginx/sites-enabled/{}".format(groupname))
+
     # symlink the php-fpm config
     if os.path.exists("/etc/php/7.1/fpm/pool.d/www.conf"):
         os.unlink("/etc/php/7.1/fpm/pool.d/www.conf")
@@ -340,41 +224,23 @@ def main(argv):
                    "/etc/php/7.1/fpm/pool.d/www.conf")
 
     # Create users
-    students = "/root/config/students.tsv"
-    StudentRecord = namedtuple(
-        "StudentRecord", "lastname, firstname, email, classname, github, "
-        "image1, team1, image2, team2, comment, week")
-
-    if (os.path.exists(students)):
-        with open(students, encoding="utf-8") as f:
+    if (os.path.exists(STUDENTS)):
+        with open(STUDENTS, encoding="utf-8") as f:
             reader = csv.reader(f, delimiter="\t")
             # skip headers
             next(reader)
             for student in map(StudentRecord._make, reader):
-
-                if 'LONE_WOLF' not in environ:
-                    groups = {
-                        t.strip().lower()
-                        for t in (student.team1, student.team2)
-                    }
-                else:
-                    # '.' is bad for databases name.
-                    groups = {student.email.replace('.', '_')}
+                groups = {
+                    t.strip().lower()
+                    for t in (student.team1, student.team2)
+                }
 
                 # Only pick the users of the given group (or admin)
                 if not groups.isdisjoint((groupname.lower(), ADMIN)):
-                    username = formatUserName(student.firstname)
-                    create_user(username, groupname, student.classname)
-                    p = multiprocessing.Process(
-                        target=init_user,
-                        args=(username, groupname),
-                        kwargs=dict(environ=environ, **student._asdict()))
-                    p.start()
-                    p.join()
-                    authorized_keys(username, student.github)
-                    sys.stderr.write("{} created.\n".format(username))
+                    authorized_keys(poweruser, student.github)
+                    sys.stderr.write("{} is {}.\n".format(poweruser, student.github))
     else:
-        sys.stderr.write("No {} file found.\n".format(students))
+        sys.stderr.write("No {} file found.\n".format(STUDENTS))
 
     sys.stderr.write("Setup is done.\n")
     return 0
