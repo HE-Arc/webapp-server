7e636afed4863743089733f9256fe4f4eda2240d
Yoan Blanc <yoan@dosimple.ch>
2017-06-21 11:31:06 +0200

    Even less Python :-)
    Signed-off-by: Yoan Blanc <yoan@dosimple.ch>

--- scripts/boot.sh
+++ scripts/boot.sh
@@ -2,14 +2,40 @@
 
 set -xe
 
+username=poweruser
+
 # regen SSH keys / passwords on each boot.
 if [ 0 -eq `ls /etc/ssh/ssh_host_* | wc -l` ]
-    then
+then
     dpkg-reconfigure openssh-server
 
     # randomize the password
-    pwgen 64 1 | chpasswd $username:$username
+    echo $username:`pwgen 64 1` | chpasswd
 fi
+
+# Environment variables
+
+export MYSQL_HOST=${MYSQL_HOST:-mysql}
+export MYSQL_PORT=${MYSQL_PORT:-3306}
+export POSTGRES_HOST=${POSTGRES_HOST:-postgres}
+export POSTGRES_PORT=${POSTGRES_PORT:-5432}
+export REDIS_HOST=${REDIS_HOST:-redis}
+export REDIS_PORT=${REDIS_PORT:-6379}
+export SMTP_HOST=${SMTP_HOST:-smtp}
+export SMTP_PORT=${SMTP_PORT:-1025}
+
+# Rails
+export SECRET_KEY_BASE=${SECRET_KEY_BASE:-`pwgen 128 1`}
+# Django
+export SECRET_KEY=${SECRET_KEY:-$SECRET_KEY_BASE}
+
+mkdir -p /etc/container_environment
+
+awk 'END { for (name in ENVIRON) {
+    if (name != "PWD" && name != "USER" && name != "HOSTNAME" && name != "PATH")
+      print ENVIRON[name] > "/etc/container_environment/"name
+  }
+}' < /dev/null
 
 # pre-setup
 if [ ! -d /var/www/config ]
@@ -17,17 +43,15 @@ if [ ! -d /var/www/config ]
     mkdir /var/www/config
     mkdir /var/www/logs
 
-    # global config
-    chpst -u $username ${CONFIG}.sh
-
-    username=poweruser
-
     uid=`id -u $username`
     gid=`id -g $username`
 
     # change the rights
     find /var/www -user root -exec chown -h $uid {} \;
     find /var/www -group root -exec chgrp -h $gid {} \;
+
+    # global config
+    chpst -u $username ${CONFIG}.sh
 
     # create the users and finalize the setup.
     /usr/local/bin/setup.py
