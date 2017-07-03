d6fd21ecfe4e2b7b43adb0f501fa823f40e0a30d
Yoan Blanc <yoan@dosimple.ch>
2017-07-03 14:51:20 +0200

    Fix rails' boot.sh regarding GEM_HOME.
    Signed-off-by: Yoan Blanc <yoan@dosimple.ch>

--- files/rails/boot.sh
+++ files/rails/boot.sh
@@ -7,6 +7,10 @@ cp /var/templates/rails/config/nginx.conf /var/www/config/nginx.conf
 cp /var/templates/rails/config/puma.rb /var/www/config/puma.rb
 cp -r /var/templates/rails/app /var/www/app
 
+USER=`id -nu`
+export GEM_HOME=/home/$USER/.gem/ruby/2.4.0
+export PATH=$PATH:$GEM_HOME/bin
+
 gem install bundler puma
 cd /var/www/app
 bundler install
