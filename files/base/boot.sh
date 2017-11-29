a730db548bdddcebbe3c3d227d143b7424f03e6a
Yoan Blanc <yoan@dosimple.ch>
2017-11-29 10:07:21 +0100

    bugfix base/boot.sh would exit with an error code
    Signed-off-by: Yoan Blanc <yoan@dosimple.ch>

--- files/base/boot.sh
+++ files/base/boot.sh
@@ -3,5 +3,5 @@
 set -xe
 
 cp /var/templates/base/README.md /var/www/
-[ ! -f /var/www/config/nginx.conf ] && cp /var/templates/base/config/nginx.conf /var/www/config/
-[ ! -d /var/www/public ] && cp -r /var/templates/base/public /var/www/
+[ ! -f /var/www/config/nginx.conf ] && cp /var/templates/base/config/nginx.conf /var/www/config/ || echo ":-)"
+[ ! -d /var/www/public ] && cp -r /var/templates/base/public /var/www/ || echo ":-)"
