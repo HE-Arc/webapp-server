29ba10e565e4bf1ffda7e1d028f14e020a889a4b
Yoan Blanc <yoan@dosimple.ch>
2019-12-20 10:37:13 +0100

    base: use dumb-init
    Signed-off-by: Yoan Blanc <yoan@dosimple.ch>

--- files/laravel/boot.sh
+++ files/laravel/boot.sh
@@ -7,7 +7,6 @@ cp /var/templates/laravel/README.md /var/www/
 [ ! -f /var/www/config/php-fpm.conf ] && cp /var/templates/laravel/config/php-fpm.conf /var/www/config/php-fpm.conf
 [ ! -f /var/www/app ] && cp -r /var/templates/laravel/app /var/www/app
 
-
 if [ ! -L /etc/php/7.2/fpm/pool.d/www.conf ]
 then
     sudo mv /etc/php/7.2/fpm/pool.d/www.conf /etc/php/7.2/fpm/pool.d/www.conf.old
